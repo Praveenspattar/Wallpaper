8f60c28a748382f4f6e77c0727641b818da631a9
Praveen <pattarpraveen15@gmail.com>
2023-06-25 16:25:02 +0530

    finished

--- app/src/main/java/com/myapps/wallpaper_app/FullimageActivity.java
+++ app/src/main/java/com/myapps/wallpaper_app/FullimageActivity.java
@@ -1,24 +1,43 @@
 package com.myapps.wallpaper_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
 import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FullimageActivity extends AppCompatActivity {
 
-    private ImageView fullimage;
+    ImageView fullimage;
+    ImageButton download;
+    Bitmap bitmap;
+    BitmapDrawable bitmapDrawable;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -28,16 +47,114 @@ public class FullimageActivity extends AppCompatActivity {
 
         fullimage = findViewById(R.id.fillImage);
         Button apply = findViewById(R.id.apply);
+        download = findViewById(R.id.download);
 
-        Glide.with(this).load(getIntent().getStringExtra("Images")).into(fullimage);
+        String img = getIntent().getStringExtra("image");
+        Glide.with(this).load(img).into(fullimage);
 
         apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setBackground();
+
+            }
+        });
+
+        // Check if the WRITE_EXTERNAL_STORAGE permission is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        } else {
+            // Permission is already granted, proceed with your code
+            setupDownloadButton();
+        }
+
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            // Check if the permission was granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with your code
+                setupDownloadButton();
+            } else {
+                // Permission denied, handle accordingly (e.g., show an error message)
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void setupDownloadButton() {
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                bitmapDrawable = (BitmapDrawable) fullimage.getDrawable();
+                bitmap = bitmapDrawable.getBitmap();
+
+                FileOutputStream fileOutputStream;
+
+                File sdCard = Environment.getExternalStorageDirectory();
+                File Directory = new File(sdCard.getAbsolutePath(),"/Download");
+                Directory.mkdir();
+
+                String filename = String.format("%d.jpg",System.currentTimeMillis());
+                File outfile = new File(Directory,filename);
+
+
+
+                try {
+
+                    fileOutputStream = new FileOutputStream(outfile);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100,fileOutputStream);
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+
+                    Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    intent.setData(Uri.fromFile(outfile));
+                    sendBroadcast(intent);
+
+                    Toast.makeText(FullimageActivity.this, "Image saved Successfully",Toast.LENGTH_SHORT).show();
+
+                } catch (RuntimeException e) {
+                    throw new RuntimeException(e);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
+
+
+    /*private void downloadImage(String img) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED){
+            //permission not granted , request it
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+            }else {
+            startImagedownload(img);
+        }
+    }
+
+    private void startImagedownload(String img) {
+        DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse("image");
+        String request = new DownloadManager.Request(uri).toString();
+
+        //set destination and file name
+
+    }*/
+
 
     private void setBackground() {
         Bitmap bitmap = ((BitmapDrawable)fullimage.getDrawable()).getBitmap();
@@ -46,6 +163,7 @@ public class FullimageActivity extends AppCompatActivity {
 
         try {
             manager.setBitmap(bitmap);
+            Toast.makeText(this,"Wallpaper Updated",Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             Toast.makeText(this, "Error" + e.getMessage(), Toast.LENGTH_SHORT).show();
             throw new RuntimeException(e);
